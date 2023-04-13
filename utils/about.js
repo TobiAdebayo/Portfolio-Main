90a3eb59e9ca76b55e97a6db706675088743b0fc
TobiAdebayo <77130933+TobiAdebayo@users.noreply.github.com>
2023-04-13 04:44:28 +0100

    Update about.js

--- utils/about.js
+++ utils/about.js
@@ -4,7 +4,7 @@ const updateAbout = [
     location: 'Synchronia Systems || Gonin-gora Kaduna',
     role: 'Mobile App Development, IoT and Product Design',
     discription:
-      'I volunteered as an intern. My role was to assist in building mobile apps for embedded systems and microcontrollers. The last project I worked on was a mobile app for a smart meter, which handles automatic recharge of light unit.',
+      'I volunteered as an intern. My role was to assist in building mobile apps for embedded systems and microcontrollers.',
   },
   {
     duration: '2018 - 2019',
@@ -18,7 +18,7 @@ const updateAbout = [
     location: 'Nigeria Society of Mining Engineers (NSME)',
     role: 'Member',
     discription:
-      'Presented a Technical Report titled Mobile App Enabled Multi Gas Detector for Underground Mines at the Annual National Conference NSME 2022 on behalf of Mineral and Petroleum Resources Engineering Department Federal Polytechnic Kaduna State.Presented a Technical Report titled Mobile App Enabled Multi Gas Detector for Underground Mines at the Annual National Conference NSME 2022 on behalf of Mineral and Petroleum Resources Engineering Department Federal Polytechnic Kaduna State.',
+      'I developed a Mobile App and a Multi Gas Detector for detecting toxic gases in Surface & Underground Mines',
   },
   {
     duration: 'Oct 2017 - Jan 2018',
