5ce03bc5df6cb32c57bf81aad9ec73f3828d64f8
TobiAdebayo <77130933+TobiAdebayo@users.noreply.github.com>
2023-04-14 14:21:44 +0100

    Update projects.js

--- utils/projects.js
+++ utils/projects.js
@@ -1,10 +1,10 @@
 const updateProject = [
   {
-    img: 'Cover.png',
+    img: 'Cover Review4Me.png',
     link: 'https://www.figma.com/file/MwRFbVwfyyclFoum4SGiJQ/Untitled?node-id=147%3A6&t=020Hu0Gk3Foqcq0m-1',
   },
   {
-    img: 'SmartMeter2.png',
+    img: 'smartMeter.png',
     link: 'https://www.figma.com/file/WtaoUYMuH19AxJSRwknhyU/Smart-Meter-(Copy)?node-id=401%3A196&t=VuWkTz0OjKMiipt9-1',
     github: 'https://github.com/emmanuelsheshi/preMeter.git',
   },
@@ -13,11 +13,11 @@ const updateProject = [
     link: 'https://www.figma.com/file/Js5u3Fdsd0UssE6SGDX2dO/Smart-Meter?node-id=301%3A182&t=PPKHYvUU6e71FMIb-1',
   },
   {
-    img: 'project-1.jpg',
+    img: 'project-1.png',
     link: 'https://www.linkedin.com/posts/oluwatobi-adebayo-4738131a1_smartmining-embeddedsystems-iotsolutions-activity-7044443564809883648-szWf?utm_source=share&utm_medium=member_desktop',
   },
   {
-    img: 'avtar2.jpg',
+    img: 'avtarr.png',
     link: 'https://oluwatobiadebayo.onrender.com',
     github: 'https://github.com/TobiAdebayo/Portfolio-Main.git',
   },
