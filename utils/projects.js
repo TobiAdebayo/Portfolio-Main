a4568e6cba781450141249e7d2dac80bb2f94546
TobiAdebayo <77130933+TobiAdebayo@users.noreply.github.com>
2023-04-16 16:01:59 +0100

    Update projects.js

--- utils/projects.js
+++ utils/projects.js
@@ -1,25 +1,25 @@
 const updateProject = [
   {
-    img: 'Cover 1.jpg',
+    img: 'Cover 1.webp',
     link: 'https://www.figma.com/proto/MwRFbVwfyyclFoum4SGiJQ/Untitled?node-id=0-3&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=0%3A3',
     github: 'https://www.figma.com/file/MwRFbVwfyyclFoum4SGiJQ/Untitled?node-id=147%3A6&t=020Hu0Gk3Foqcq0m-1',
   },
   {
-    img: 'SmartMeter 1.jpg',
+    img: 'SmartMeter 1.webp',
     link: 'https://github.com/emmanuelsheshi/preMeter.git',
     github:'https://www.figma.com/file/WtaoUYMuH19AxJSRwknhyU/Smart-Meter-(Copy)?node-id=401%3A196&t=VuWkTz0OjKMiipt9-1',
   },
   {
-    img: 'SmartMeterCover 1.jpg',
+    img: 'SmartMeterCover 1.webp',
     link: ' https://www.figma.com/proto/Js5u3Fdsd0UssE6SGDX2dO/Smart-Meter?node-id=119-112&scaling=scale-down&page-id=0%3A1&starting-point-node-id=308%3A300',
     github: 'https://www.figma.com/file/Js5u3Fdsd0UssE6SGDX2dO/Smart-Meter?node-id=301%3A182&t=PPKHYvUU6e71FMIb-1',
   },
   {
-    img: 'project-1.jpg',
+    img: 'project-1.webp',
     link: 'https://www.linkedin.com/posts/oluwatobi-adebayo-4738131a1_smartmining-embeddedsystems-iotsolutions-activity-7044443564809883648-szWf?utm_source=share&utm_medium=member_desktop',
   },
   {
-    img: 'avata 1.jpg',
+    img: 'avata 1.webp',
     link: 'https://oluwatobiadebayo.onrender.com',
     github: 'https://github.com/TobiAdebayo/Portfolio-Main.git',
   },
